5cd9596952bee9caa90f34005034f4981827f380
davis stanko <davisbvstanko@gmail.com>
2022-04-24 14:18:36 -0400

    memes

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
 # bot.py
 import os
-
+import os.path
+import random
+import time
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+SERVER = os.getenv('DISCORD_SERVER')
 
 client = discord.Client()
 
-@client.event
+# Count memes for random meme function
+number_of_memes = 0
+dir = "memes"
+for path in os.listdir(dir):
+    if os.path.isfile(os.path.join(dir, path)):
+        number_of_memes += 1
+print(number_of_memes)
+
+
+@client.event  # Connect to discord
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == SERVER:
+            break
+
+    print(
+        f'{client.user} is connected to the following guilds:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+
+@client.event  # Send message reply
+async def on_message(message):
+    # If message is from bot, ignore
+    if message.author == client.user:
+        return
+
+    if message.content.lower() == '!meme':
+        meme = random.randint(1, number_of_memes)
+        try:
+            file = discord.File(f"memes/{meme}.gif")
+        except:
+            try:
+                file = discord.File(f"memes/{meme}.mp4")
+            except:
+                file = discord.File(f"memes/{meme}.png")
+        start_time = time.time()
+        await message.channel.send(file=file, content="")
+        end_time = time.time()
+        total_time = round(end_time - start_time, 4)
+        await message.channel.send(f"Took {total_time} seconds to send meme.")
+
 
 client.run(TOKEN)
