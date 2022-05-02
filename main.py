cacfbe6e6b3ab208525e1e40348e1bd9936db669
davis stanko <davisbvstanko@gmail.com>
2022-05-01 22:17:42 -0400

    go leafs go for nic

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 SERVER = os.getenv('DISCORD_SERVER')
 ADMIN = os.getenv('DISCORD_ADMIN')
 SPOTIFY_PROFILE = os.getenv('SPOTIFY_PROFILE')
+NIC = os.getenv('NIC_PROFILE')
 
 client = discord.Client()
 
@@ -94,6 +95,10 @@ async def on_message(message):
         path = f"{directory}/{attachment}" # Get path to file
         final = discord.File(path) # Create file object
         await message.channel.send(file=final) # Send file
+        
+    # Made for a freind
+    if message.author == NIC:
+        await message.channel.send(content="Go Leafs Go!")
 
 
 client.run(TOKEN)
