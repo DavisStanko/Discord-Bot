81673f0212cd30814da196d81c331c913d8621e5
davis stanko <davisbvstanko@gmail.com>
2022-05-03 23:40:24 -0400

    typos

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ songhelpmessage = (f"Add a playlist to the end of the !song command to get a ran
 # Space in front of first command is intentional
 commandlist = [" !help", "!info", "!song", "!meme", "!object", "!frog", "!cat", "!shrigma", "!chill"]
 commandlist = ", ".join(commandlist).replace(',', '\n') # Join commands into one string and format it
-helpmessage = (f"I react to the following commands:\n{commandlist}\nYou can add \"amount\" to the end of most commands to get the total number of possible outputs.\nDislike a file attachment? If a messagefrom me gets the 👎 reaction {votes} times the attachment will automatically be deleted from my server.")
+helpmessage = (f"I react to the following commands:\n{commandlist}\nYou can add \"amount\" to the end of most commands to get the total number of possible outputs.\nDislike a file attachment? If a message from me gets the 👎 reaction {votes} times, the attachment will automatically be deleted from my server.")
 
 
 
