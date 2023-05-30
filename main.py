c123cf7ee2dba30568265162d76581e91b6e4bcd
davis stanko <davisbvstanko@gmail.com>
2023-05-30 17:49:49 -0400

    accept more request formats

--- main.py
+++ main.py
@@ -86,6 +86,10 @@ async def on_message(message):
     elif request.startswith('!'):
         # Remove the '!' from the request
         request = request[1:]
+        # Remove spaces from the request
+        request = request.replace(" ", "")
+        # .lower the request
+        request = request.lower()
 
         if request == "help":
             reply = "I can help you with the following commands:\n" \
