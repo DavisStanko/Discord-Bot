bfe490f0d9f12db778424e048a3b1153e0cab4b0
davis stanko <davisbvstanko@gmail.com>
2023-05-30 17:49:13 -0400

    cleaning

--- main.py
+++ main.py
@@ -1,97 +1,92 @@
 import os
 import random
 import discord
-from dotenv import load_dotenv  # Loads the .env file
+from dotenv import load_dotenv
 import re
 import requests
 import json
 import html
 import asyncio
 
-
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN') # Bot token
-SERVER = os.getenv('DISCORD_SERVER')  # Server ID
-ADMIN = os.getenv('DISCORD_ADMIN')  # Admin ID
+TOKEN = os.getenv('DISCORD_TOKEN')
+SERVER = os.getenv('DISCORD_SERVER')
+ADMIN = os.getenv('DISCORD_ADMIN')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-# get a list of all the folders in a directory
+# Get a list of all the folders in a directory
 def get_child_folders(path):
     return [entry for entry in os.listdir(path) if os.path.isdir(os.path.join(path, entry))]
 
-# get the media paths
+# Get the media paths
 main_dir = os.path.dirname(os.path.realpath(__file__))
 content_path = os.path.join(main_dir, "internet")
 
-# get the command lists
+# Get the command lists
 content_commands = sorted(["!" + command for command in get_child_folders(content_path)])
 
-# format the command lists
+# Format the command lists
 content_commands = "\n".join([f"`{command}`" for command in content_commands])
 
-# utility help message
+# Utility help message
 utility_commands = "`!info` - Links to my GitHub page.\n" \
-                    "`!NdM` - Rolls N M-sided dice where N and M are positive integers.\n" \
+                   "`!NdM` - Rolls N M-sided dice where N and M are positive integers.\n"
 
-# check if a string is in NdM format
+# Check if a string is in NdM format
 def is_valid_dice_format(string):
     pattern = r"\d+d\d+"
     match = re.fullmatch(pattern, string)
     return match is not None
 
-# get a random trivia question
-import requests
-import json
-
-# Function to fetch a random trivia question with its answer from OTDB API
+# Get a random trivia question
 def get_random_question():
     url = "https://opentdb.com/api.php?amount=1&type=multiple"
     response = requests.get(url)
     data = json.loads(response.text)
-    
+
     question = data['results'][0]
     question_text = html.unescape(question['question'])
     correct_answer = html.unescape(question['correct_answer'])
     incorrect_answers = [html.unescape(answer) for answer in question['incorrect_answers']]
     answers = incorrect_answers + [correct_answer]
     random.shuffle(answers)
-    
+
     question_data = {
         'question': question_text,
         'answers': answers,
         'correct_answer': correct_answer
     }
-    
+
     return question_data
 
-@client.event  # Connect to discord
+@client.event
 async def on_ready():
-    # connects to servers from .env
+    # Connects to servers from .env
     for guild in client.guilds:
         if guild.name == SERVER:
             break
 
-    # Prints out infor for debugging
+    # Print out information for debugging
     print(f'{client.user} is connected to the following servers:\n{SERVER}')
 
 
-@client.event  # Send message reply
+@client.event
 async def on_message(message):
     request = message.content.lower()
 
-    # If message is from bot, ignore
+    # If message is from the bot, ignore
     if message.author == client.user:
         return
 
     # If message is a command
-    elif request.startswith('!'):   
-        # remove the ! from the request
+    elif request.startswith('!'):
+        # Remove the '!' from the request
         request = request[1:]
-        
+
         if request == "help":
             reply = "I can help you with the following commands:\n" \
                     "`!help` - Displays this help message.\n" \
@@ -100,17 +95,17 @@ async def on_message(message):
                     "`!utility` - Lists utility commands."
             await message.channel.send(reply)
             return
-            
+
         if request == "utility":
             reply = f"I react to the following utility commands:\n{utility_commands}"
             await message.channel.send(reply)
             return
 
-        if request in "content":
+        if request == "content":
             reply = f"I react to the following content commands by sending a random media file from the specified directory:\n{content_commands}"
             await message.channel.send(reply)
             return
-        
+
         if request == "games":
             reply = f"I react to the following game commands:\n" \
                     "`!trivia` - Starts a game of trivia."
@@ -121,30 +116,29 @@ async def on_message(message):
             reply = f"https://github.com/DavisStanko/Discord-Bot"
             await message.channel.send(reply)
             return
-        
+
         # Trivia
-        if message.content == "!trivia":            
+        if message.content == "!trivia":
             question_data = get_random_question()
             question = question_data['question']
             answers = question_data['answers']
             correct_answer = question_data['correct_answer']
-            
 
-            # prompt to answer via number
+            # Prompt to answer via number
             prompt = "Please answer by sending the number of the correct answer within 10 seconds."
-            # replace &quot; with "
+            # Replace &quot; with "
             question = question.replace("&quot;", "\"")
-            # format the question
+            # Format the question
             question = f"**{question}**\n"
-            # format the answers
+            # Format the answers
             answers = [f"{i+1}. {answer}" for i, answer in enumerate(answers)]
-            # combine the prompt question and answers
-            reply = question + "\n".join(answers) + "\n" + prompt     
-            
-            # send the question
+            # Combine the prompt, question, and answers
+            reply = question + "\n".join(answers) + "\n" + prompt
+
+            # Send the question
             await message.channel.send(reply)
-            
-            # check if the answer is correct
+
+            # Check if the answer is correct
             def check_answer(m):
                 return m.author == message.author and m.channel == message.channel
 
@@ -164,35 +158,32 @@ async def on_message(message):
             except asyncio.TimeoutError:
                 await message.channel.send("Time's up! The correct answer is: " + correct_answer)
 
-            
-
-        
-        # content
+        # Content
         if request in content_commands:
-            # get the path to the folder
+            # Get the path to the folder
             folder_path = os.path.join(content_path, request)
-            # get a list of all the files in the folder
+            # Get a list of all the files in the folder
             files = os.listdir(folder_path)
-            # get a random file from the list
+            # Get a random file from the list
             file = random.choice(files)
-            # get the path to the file
+            # Get the path to the file
             file_path = os.path.join(folder_path, file)
-            # send the file
-            await message.channel.send(f"here is your {request}!", file=discord.File(file_path))
+            # Send the file
+            await message.channel.send(f"Here is your {request}!", file=discord.File(file_path))
             return
-        
-        # utility
-        # if request is in NdM format
+
+        # Utility
+        # If request is in NdM format
         if is_valid_dice_format(request):
-            # split the string into N and M
+            # Split the string into N and M
             N, M = request.split("d")
-            # convert N and M to integers
+            # Convert N and M to integers
             N = int(N)
             M = int(M)
-            # roll the dice
+            # Roll the dice
             rolls = [random.randint(1, M) for i in range(N)]
-            # format the reply
-            reply = f"you rolled {N}d{M} and got {sum(rolls)} ({rolls})"
+            # Format the reply
+            reply = f"You rolled {N}d{M} and got {sum(rolls)} ({rolls})"
             await message.channel.send(reply)
             return
 
